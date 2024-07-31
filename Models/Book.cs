a649d6cd9da321cb8c7c87c30321105c1569c3bf
ramunasdapkus@gmail.com <ramunasdapkus@gmail.com>
2024-07-31 21:56:24 +0300

    Added Models

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Book_rew.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime PublishedTime { get; set; }
+        
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Book_rew.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public Book BookId {  get; set; }
+        public string ReviewerName { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
